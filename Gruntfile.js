9ac8f5c2cc21e609ed625b46ce63349532409751
Gildo Giuliani <gildo.giuliani@gmail.com>
2017-03-26 17:08:52 +0200

    v.0.1.0
    Grunt tasks added to the project

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,41 @@
 module.exports = function (grunt) {
 
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json');
+        pkg: grunt.file.readJSON('package.json'),
+        clean: ['dist'],
+        copy: {
+            dist: {
+                files: [
+                    {
+                        src: ['src/demo.css*'],
+                        dest: 'dist/demo.css'
+                    }
+            ]
+            }
+        },
+        uglify: {
+            dist: {
+                files: {
+                    'dist/jquery.ga-trackers.pack.min.js': ['src/jquery.ga-trackers.onscroll.js', 'src/jquery.ga-trackers.js'],
+                    'dist/jquery.ga-trackers.min.js': ['src/jquery.ga-trackers.js'],
+                    'dist/jquery.ga-trackers.onscroll.min.js': ['src/jquery.ga-trackers.onscroll.js'],
+                }
+            }
+        },
+        processhtml: {
+            dist: {
+              files: {
+                'dist/index.html': ['src/index.html']
+              }
+            }
+        }
     });
 
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-processhtml');
 
-    grunt.registerTask('default', ['jshint']);
+    grunt.registerTask('default', ['clean','copy','uglify','processhtml']);
 
 };
